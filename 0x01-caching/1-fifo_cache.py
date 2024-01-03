537b0cae3d14ee4cc6c0c4220abae8c8d20dad93
thewisepeter <rempeterwisdom@gmail.com>
2024-01-04 00:28:18 +0300

    solution to 2-lifo_cache.py

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -37,12 +37,13 @@ class FIFOCache(BaseCaching):
         '''
 
         if key is not None and item is not None:
-            self.cache_data[key] = item
+            if key not in self.cache_data:
+                if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                    last_key, _ = self.cache_data.popitem(True)
+                    print("DISCARD:", last_key)
 
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                first_key, _ = next(iter(self.cache_data.items()))
-                self.cache_data.pop(first_key)
-                print(f"DISCARD: {first_key}")
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=True)
 
     def get(self, key):
         '''
