1a0a733c0b6e3160855220a897e9beb262ae6ca8
thewisepeter <rempeterwisdom@gmail.com>
2024-01-10 14:11:45 +0000

    fixed pycode 2-app

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -20,12 +20,14 @@ app.config.from_object(Config)
 app.url_map.strict_slashes = False
 babel = Babel(app)
 
+
 @babel.localeselector
 def get_locale() -> str:
-    ''' 
+    '''
         gets locale for a web page
     '''
     return request.accept_languages.best_match(app.config["LANGUAGES"])
+
 
 @app.route('/')
 def get_index() -> str:
