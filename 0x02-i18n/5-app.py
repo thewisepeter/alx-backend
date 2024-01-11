f49526d2109ab16dd1a7d181902e8a2eb090f121
thewisepeter <rempeterwisdom@gmail.com>
2024-01-11 10:54:17 +0300

    reworked get_locale

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -46,14 +46,9 @@ def before_request() -> None:
 @babel.localeselector
 def get_locale() -> str:
     ''' gets locale for a web page '''
-    queries = request.query_string.decode('utf-8').split('&')
-    query_table = dict(map(
-        lambda x: (x if '=' in x else '{}='.format(x)).split('='),
-        queries,
-        ))
-    if 'locale' in query_table:
-        if query_table['locale'] in app.config["LANGUAGES"]:
-            return query_table['locale']
+    locale = request.args.get('locale', '')
+    if locale in app.config["LANGUAGES"]:
+        return locale
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
