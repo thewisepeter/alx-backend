81a7a86553cb793db1ac13ce6174365b6e6a3f28
thewisepeter <rempeterwisdom@gmail.com>
2024-02-06 08:57:33 +0000

    4-redis_advanced_op.js implementation

--- 0x03-queuing_system_in_js/4-redis_advanced_op.js
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -10,48 +10,25 @@ client
     console.log(`Redis client not connected to the server: ${error}`);
   });
 
+// Function to set multiple fields in a hash
+function setHashFields(hashName, fields) {
+  Object.entries(fields).forEach(([field, value]) => {
+    client.hset(hashName, field, value, print);
+  });
+}
+
+// Define the hash fields and their values
+const hashFields = {
+  'Portland': 50,
+  'Seattle': 80,
+  'New York': 20,
+  'Bogota': 20,
+  'Cali': 40,
+  'Paris': 2
+};
+
 // Create Hash
-client.hset(
-  'HolbertonSchools',
-  'Portland',
-  50,
-  print
-);
-
-client.hset(
-  'HolbertonSchools',
-  'Seattle',
-  80,
-  print
-);
-
-client.hset(
-  'HolbertonSchools',
-  'New York',
-  20,
-  print
-);
-
-client.hset(
-  'HolbertonSchools',
-  'Bogota',
-  20,
-  print
-);
-
-client.hset(
-  'HolbertonSchools',
-  'Cali',
-  40,
-  print
-);
-
-client.hset(
-  'HolbertonSchools',
-  'Paris',
-  2,
-  print
-);
+setHashFields('HolbertonSchools', hashFields);
 
 // Display Hash
 client.hgetall('HolbertonSchools', (err, reply) => {
